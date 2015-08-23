d204fea5e5f0cceea7036f8bec2ab656703cd8f4
Anastasiy Tovstik <archianastasius@gmail.com>
2015-08-23 23:16:39 +0300

    Developing front end and back end

--- src/main/java/com/rebirthlab/gradebook/service/TaskFacadeREST.java
+++ src/main/java/com/rebirthlab/gradebook/service/TaskFacadeREST.java
@@ -73,12 +73,13 @@ public class TaskFacadeREST extends AbstractFacade<Task> {
         } 
         
         //Initialises student attendance
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         Calendar calendar = Calendar.getInstance();
         Date startDate = task.getStartDate();
         calendar.setTime(startDate);
-        calendar.add(Calendar.HOUR, 8);
-        int firstDayOfWeek = calendar.getFirstDayOfWeek();
+        calendar.setTimeZone(TimeZone.getTimeZone("EDT"));
         
+        int firstDayOfWeek = calendar.getFirstDayOfWeek();      
         int startDateDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
         calendar.add(Calendar.DAY_OF_MONTH, - startDateDayOfWeek + firstDayOfWeek);
         
