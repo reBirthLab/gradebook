e2ac25790b4b98c835f48f53bffdca7e0ee6ef7b
Anastasiy Tovstik <anastasiy.tovstik@gmail.com>
2018-03-05 22:49:05 +0200

    Added oauth-service module with basic setup

---
+++ services/oauth-service/src/main/java/com/rebirthlab/gradebook/auth/Application.java
@@ -0,0 +1,18 @@
+package com.rebirthlab.gradebook.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * Created by Anastasiy
+ */
+@SpringBootApplication
+@EnableAuthorizationServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
